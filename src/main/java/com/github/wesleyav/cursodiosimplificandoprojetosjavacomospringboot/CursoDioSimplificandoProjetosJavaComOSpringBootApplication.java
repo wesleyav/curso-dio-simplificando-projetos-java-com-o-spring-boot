4918641644c59f429a2f252aa98d195f0697a6be
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-28 17:55:30 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/com/github/wesleyav/cursodiosimplificandoprojetosjavacomospringboot/CursoDioSimplificandoProjetosJavaComOSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.cursodiosimplificandoprojetosjavacomospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDioSimplificandoProjetosJavaComOSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDioSimplificandoProjetosJavaComOSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wesleyav/cursodiosimplificandoprojetosjavacomospringboot/CursoDioSimplificandoProjetosJavaComOSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.cursodiosimplificandoprojetosjavacomospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDioSimplificandoProjetosJavaComOSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
